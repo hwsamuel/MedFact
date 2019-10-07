af90fc3240cc3b698769251fc02c1c869dc3d82d
Hamman Samuel <hwsamuel@ualberta.ca>
2019-10-07 07:28:26 -0400

    Added NDCG inspector and refactored

--- trip.py
+++ trip.py
@@ -2,24 +2,24 @@ from requests import get
 from xml.etree import ElementTree
 from datetime import datetime
 from enum import Enum 
+from nltk.stem import PorterStemmer
+from nltk import word_tokenize
 
 from article import *
 from pyramid import *
 
+import numpy as np
+
 TRIPWEB_KEY = ''
 
-'''
-Sorting options in Trip
-'''
+""" Sorting options in Trip """
 class SortResults(Enum):
-    quality = ''
+    quality = '' # Default value based on best NDCG results (arbitrary)
     date = 'y'
     relevance = 't'
     popularity = 'v12m'
 
-'''
-Search field options in Trip
-'''
+""" Search field options in Trip """
 class SearchField(Enum):
 	title = 'title:'
 	anywhere = ''
@@ -28,14 +28,14 @@ class SearchField(Enum):
 Queries the Trip database's web service based on specified query and fields and returns results based on sort order
 
 keywords (list) 	- Python list of keywords
-proximity (int) 	- Distance between keywords
+proximity (int) 	- Distance between keywords (PRO account on TRIP required)
 field (SearchField) - Field to search
 sorter (SortResults)- Sort order
 
 return (Article)	- List of Article objects containing matches with title, evidence category, evidence weight, source, year published, and url
 '''
 def query(keywords, proximity=20, field=SearchField.anywhere.value, sorter=SortResults.quality.value):
-	keywords = " ".join(keywords)
+	keywords = " ".join(keywords).lower()
 	query = '%s"%s"~%s' % (field, keywords, proximity)
 	
 	end_point = 'https://www.tripdatabase.com/search/xml'
@@ -59,17 +59,58 @@ def query(keywords, proximity=20, field=SearchField.anywhere.value, sorter=SortR
 		results.append(Article(title, category, weight, source, year, url))
 	return results
 
-'''
-Unit tests
-'''
+""" NDCG arbitary tests to select best sorting method from TRIP results """
+def ndcg():
+	keywords = ["autism", "vaccinations"] # Search query keywords
+	
+	# Inner function to compute NDCG at k
+	def ndcg_at_k(r, k, method=0):
+	    dcg_max = dcg_at_k(sorted(r, reverse=True), k, method)
+	    if not dcg_max:
+	        return 0.
+	    return dcg_at_k(r, k, method) / dcg_max
+
+    # Inner helper function to compute DCG at k
+	def dcg_at_k(r, k, method=0):
+		r = np.asfarray(r)[:k]
+		if r.size:
+		    if method == 0:
+		        return r[0] + np.sum(r[1:] / np.log2(np.arange(2, r.size + 1)))
+		    elif method == 1:
+		        return np.sum(r / np.log2(np.arange(2, r.size + 2)))
+		    else:
+		        raise ValueError()
+		return 0.
+
+	# Inner function to score query results by keyword hits
+	def scores(matches):
+		porter = PorterStemmer()
+		result = []
+		for match in matches:
+			title_stems = [porter.stem(t.lower()) for t in word_tokenize(match.title)]
+			keyword_stems = [porter.stem(k.lower()) for k in keywords]
+			common = len(list(set(title_stems) & set(keyword_stems)) )
+			result.append(common)
+		return result
+	
+	# Queries TRIP and computes score for results based on different sorting orders
+	r1 = scores(query(keywords, field=SearchField.anywhere.value, sorter=SortResults.quality.value))
+	r2 = scores(query(keywords, field=SearchField.anywhere.value, sorter=SortResults.relevance.value))
+	r3 = scores(query(keywords, field=SearchField.anywhere.value, sorter=SortResults.date.value))
+	r4 = scores(query(keywords, field=SearchField.anywhere.value, sorter=SortResults.popularity.value))
+
+	# Prints results for comparison
+	print 'Quality', ndcg_at_k(r1, 5)
+	print 'Relevance', ndcg_at_k(r2, 5)
+	print 'Date', ndcg_at_k(r3, 5)
+	print 'Popularity', ndcg_at_k(r4, 5)
+
+""" Sanity test """
 def test():
-	results = query(["apricot", "cancer"])
+	results = query(["autism", "vaccinations"])
 	for result in results:
 		print result.title, result.category, result.weight, result.source, result.year, result.url
 		print
 
-'''
-Main execution point
-'''
 if __name__ == '__main__':
 	test()
