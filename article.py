f997c2c5a1ae413989ae22943a5679a731878402
Hamman Samuel <hwsamuel@ualberta.ca>
2019-10-14 14:54:08 -0400

    Added best and other samplers

--- article.py
+++ article.py
@@ -1,6 +1,9 @@
 from textblob import TextBlob
 from nltk import word_tokenize
+from nltk import flatten
 from string import punctuation
+from enum import Enum 
+from random import shuffle
 
 '''
 Represents articles returned from Trip search
@@ -13,6 +16,13 @@ source (str)	- The name of the journal or website where the article is from
 year (str)		- Year the article was published
 url (str)		- A link to the article (in some cases would provide full text access)
 '''
+
+class Sampler(Enum):
+	first = 0		# Choose first-n sentences
+	last = 1		# Choose last-n sentences
+	random = 2		# Choose random-n sentences
+	best = 3		# Choose sentences with most keyword hits
+
 class Article():
 	def __init__(self, title, body, category, weight, source, year, url):
 		self.title = title
@@ -23,24 +33,41 @@ class Article():
 		self.year = year
 		self.url = url
 
-	def extract(self, keywords, threshold = 3):
+	def extract(self, keywords, threshold = 3, sampler = Sampler.last.value):
 		"""
 		Extracts the relevant phrases to use for comparison with an incoming query for computing veracity
 
-		keywords (list)	- Query of keywords
-		threshold (int)	- Returns the last-n sentences (-1 for all sentences)
-		return (list)	- List of phrases and sentences to use for veracity computation
+		keywords (list)		- Query of keywords
+		threshold (int)		- Controls number of sentences to return (-1 for all sentences)
+		sampler(Sampler) 	- Returns first-n, last-n, random-n or best-n number of sentences
+
+		return (list)		- List of phrases and sentences to use for veracity computation
 		"""
 		
 		sentences = TextBlob(self.body).sentences # Extract sentences from article body
-		relevant = []
+		relevant = {}
 		for sentence in sentences:
 			sentence = str(sentence).lower().strip()
 			tokens = word_tokenize(sentence)
 			tokens = [t.lower() for t in tokens if t not in punctuation]
 
-			common = [value for value in keywords if value in tokens]
-			if len(common) > 0: relevant.append(' '.join(tokens))
+			common = len([value for value in keywords if value in tokens])
+			if common == 0: continue
+			if common not in relevant.keys(): relevant[common] = []
+			relevant[common].append(' '.join(tokens))
 
-		if threshold == -1: return relevant
-		else: return relevant[-threshold:]
+		if threshold == -1: return flatten(relevant.values())
+
+		if sampler == Sampler.last.value:
+			return flatten(relevant.values())[-threshold:]
+		elif sampler == Sampler.first.value:
+			return flatten(relevant.values())[:threshold]
+		elif sampler == Sampler.random.value:
+			shuffle(flatten(relevant.values()))
+			return relevant[-threshold:]
+		elif sampler == Sampler.best.value:
+			result = []
+			for key in sorted(relevant.keys(), reverse=True): # Sort by keyword hits
+				result.extend(relevant[key])
+				if len(result) >= threshold: break
+			return result[:threshold]
